c517d2f251d4ec6d7fce1b10dd67083e89bbeb7d
moinuddin95 <moinuddinshaikh173@gmail.com>
2025-05-10 15:49:18 -0400

    Added tab selection functionality

--- Portfolio/src/components/Main/Main.tsx
+++ Portfolio/src/components/Main/Main.tsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import Work from "../Work/Work";
 import "./Main.css";
 
 function Main() {
+  const [selectedTab, setSelectedTab] = useState(0);
+  // 0 -> Work
+  // 1 -> Education
+  // 2 -> Projects
   return (
     <main>
       <nav>
-        <button>
+        <button className={selectedTab === 0 ? "selected" : ""} 
+          onClick={() => setSelectedTab(0)}>
           <h3>Work</h3>
         </button>
-        <button>
+        <button className={selectedTab === 1 ? "selected" : ""} 
+          onClick={() => setSelectedTab(1)}>
           <h3>Education</h3>
         </button>
-        <button>
+        <button className={selectedTab === 2 ? "selected" : ""} 
+          onClick={() => setSelectedTab(2)}>
           <h3>Projects</h3>
         </button>
       </nav>
