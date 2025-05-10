94d64c825905c7bce00adb9df23a36954993f15f
moinuddin95 <moinuddinshaikh173@gmail.com>
2025-05-09 21:53:31 -0400

    Added the main tabs

---
+++ Portfolio/src/components/Main/Main.tsx
@@ -0,0 +1,21 @@
+import "./Main.css";
+
+function Main() {
+  return (
+    <main>
+      <nav>
+        <button>
+          <h3>Work</h3>
+        </button>
+        <button>
+          <h3>Education</h3>
+        </button>
+        <button>
+          <h3>Projects</h3>
+        </button>
+      </nav>
+    </main>
+  );
+}
+
+export default Main;
