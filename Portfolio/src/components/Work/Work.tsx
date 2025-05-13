17d5a184902b90932bb5b90e63d39b271c77b45a
moinuddin95 <moinuddinshaikh173@gmail.com>
2025-05-12 20:33:08 -0400

    Added work data

--- Portfolio/src/components/Work/Work.tsx
+++ Portfolio/src/components/Work/Work.tsx
@@ -1,11 +1,20 @@
-import "./Work.css"
+import "./Work.css";
+import data from "../../models/work.json";
 
 function Work() {
   return (
     <div id="work">
-
+      {data.map((item, index) => (
+        <div key={index}>
+          <h2>{item.organization}</h2>
+          <h3>{item.position}</h3>
+          <p>{item.start_date}</p>
+          <p>{item.end_date}</p>
+          <p>{item.responsibilities}</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Work
+export default Work;
