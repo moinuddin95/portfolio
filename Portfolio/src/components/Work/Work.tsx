487d5ab6c7440cca185c103123bc2a86bd13396c
moinuddin95 <moinuddinshaikh173@gmail.com>
2025-05-17 23:32:33 -0400

    Redesigned UI and added headshot

--- Portfolio/src/components/Work/Work.tsx
+++ Portfolio/src/components/Work/Work.tsx
@@ -5,12 +5,21 @@ function Work() {
   return (
     <div id="work">
       {data.map((item, index) => (
-        <div key={index}>
-          <h2>{item.organization}</h2>
+        <div key={index} className="work card">
           <h3>{item.position}</h3>
-          <p>{item.start_date}</p>
-          <p>{item.end_date}</p>
-          <p>{item.responsibilities}</p>
+          <div className="header">
+            <h4>{item.organization}</h4>
+            <p>
+              {item.start_date} - {item.end_date}
+            </p>
+          </div>
+          <ul>
+            {item.responsibilities.map((responsibility, index) => (
+              <li key={index}>
+                <p> {responsibility} </p>
+              </li>
+            ))}
+          </ul>
         </div>
       ))}
     </div>
