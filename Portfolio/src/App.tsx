8748d6c32b7b1db781d4cafe1ae2f8dc0deab6ea
moinuddin95 <moinuddinshaikh173@gmail.com>
2025-05-23 22:55:49 -0400

    Added mouse pointer animation completes #3

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -2,15 +2,52 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import introAnimationHook from "./hooks/introAnimationHook";
+import { useEffect } from "react";
 
 function App() {
-
   introAnimationHook("header > *, main > *");
+
+  useEffect(() => {
+    const circles = document.querySelectorAll(".circle");
+    let delay = 1;
+    circles.forEach((circle, index) => {
+      const circleElem = circle as HTMLElement;
+      circleElem.style.scale = `${(circles.length - index) / circles.length}`;
+      circleElem.style.transition = `top ${delay}ms ease, left ${delay}ms ease`;
+      delay += 15;
+      const moveCircles = (event: MouseEvent) => {
+        const pointerBorder = 10;
+        if (
+          event.clientX <= pointerBorder ||
+          event.clientX >= window.innerWidth - pointerBorder ||
+          event.clientY <= pointerBorder ||
+          event.clientY >= window.innerHeight - pointerBorder
+        ) {
+          circleElem.style.display = "none";
+          return;
+        }
+        circleElem.style.display = "block";
+        circleElem.style.top = `${event.clientY}px`;
+        circleElem.style.left = `${event.clientX}px`;
+      };
+      window.addEventListener("mousemove", moveCircles);
+    });
+  }, []);
 
   return (
     <>
-      <Header />
-      <Main />
+      <div id="background">
+        <Header />
+        <Main />
+      </div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
     </>
   );
 }
