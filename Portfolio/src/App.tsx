f95909b59f8ba43d49d1644cc7abee54f7fcd840
moinuddin95 <moinuddinshaikh173@gmail.com>
2025-11-17 23:03:17 -0500

    Update intro animation

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -7,7 +7,7 @@ function introAnimationHook(query: string){
   useEffect(() => {
     const childElem = document.querySelectorAll(query);
     for(let i = 0; i < childElem.length; i++){
-      setTimeout(() => {childElem[i].classList.add("introAnimation");}, i * 400);
+      setTimeout(() => {childElem[i].classList.add("introAnimation");}, i * 100);
     }
   }, []);
 }
